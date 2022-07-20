77dd418a4d1609cf8c5493e1790aad6610e6bf88
Omar <ooriz@fi.uba.ar>
2022-07-20 17:34:50 -0300

    Incluido un marco para la busqueda con ID

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class App(tk.Tk):
 
                 # label SUCURSAL
                 self.label = ttk.Label(self.card, text='Sucursal nro 1923')
-                self.label.pack(pady = ESPACIO_Y)
+                self.label.pack(pady = ESPACIO_Y, anchor= W)
 
                 # BOTONES
                 self.BuscarRegistro = ttk.Button(self.card,
@@ -41,21 +41,24 @@ class App(tk.Tk):
                 self.BuscarRegistro.pack(pady = ESPACIO_Y, anchor= W)
 
                 self.IngresarDueño = ttk.Button(self.card,
-                                                text='Ingresar dueño...',
+                                                text='Ingresar nuevo dueño',
                                                 style='Accent.TButton')
                 self.IngresarDueño.pack(pady = ESPACIO_Y, anchor= W)
 
                 self.IngresarMascota = ttk.Button(self.card,
-                                                text='Ingresar mascota...',  
+                                                text='Ingresar nueva mascota',  
                                                 style='Accent.TButton')
                 self.IngresarMascota.pack(pady = ESPACIO_Y, anchor= W)
 
+                # MARCO BUSQUEDA
+                self.MarcoBusqueda = ttk.Frame(self.card, style='Card', padding=(5, 6, 7, 8))
+                self.MarcoBusqueda.pack(pady = ESPACIO_Y)
 
-                self.label2 = ttk.Label(self.card, text='Busqueda con ID único de paciente')
+                self.label2 = ttk.Label( self.MarcoBusqueda, text='Busqueda con ID único de paciente')
                 self.label2.pack(pady = ESPACIO_Y)
 
                 self.VarTexto = tk.StringVar()
-                self.EntradaID = ttk.Entry(self.card, textvariable = self.VarTexto)
+                self.EntradaID = ttk.Entry(self.MarcoBusqueda, textvariable = self.VarTexto)
                 self.EntradaID.pack(pady = ESPACIO_Y)
                 self.VarTexto.set("ID de mascota...")
                 self.EntradaID.bind('<Button-1>', lambda e: self.borrar_texto(e))

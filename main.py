520258ae02875664e3994fd783ecbc26a32de5b1
Omar <ooriz.fi@fi.uba.ar>
2022-07-20 20:23:40 -0300

    Funcionalidad de Botones de Ingresar dueño y mascota. Nuevas clases para las ventanas de ingresar dueño y mascota. Nueva clase para entry custom.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -40,14 +41,17 @@ class App(tk.Tk):
                                                 style='Accent.TButton')
                 self.BuscarRegistro.pack(pady = ESPACIO_Y, anchor= W)
 
+                self.EstadoIngresarDueño = False
                 self.IngresarDueño = ttk.Button(self.card,
                                                 text='Ingresar nuevo dueño',
-                                                style='Accent.TButton')
+                                                style='Accent.TButton',
+                                                command= lambda: self.ingresar_dueño())
                 self.IngresarDueño.pack(pady = ESPACIO_Y, anchor= W)
 
                 self.IngresarMascota = ttk.Button(self.card,
                                                 text='Ingresar nueva mascota',  
-                                                style='Accent.TButton')
+                                                style='Accent.TButton',
+                                                command= lambda: self.ingresar_mascota())
                 self.IngresarMascota.pack(pady = ESPACIO_Y, anchor= W)
 
                 # MARCO BUSQUEDA
@@ -57,21 +61,142 @@ class App(tk.Tk):
                 self.label2 = ttk.Label( self.MarcoBusqueda, text='Busqueda con ID único de paciente')
                 self.label2.pack(pady = ESPACIO_Y)
 
+                self.EntradaID = EntryCustom(self.MarcoBusqueda, text='ID de mascota')
+                self.EntradaID.pack(anchor= S)
+
+        def ingresar_dueño(self):
+                self.IngresoDueño = IngresoDueño()
+
+        def ingresar_mascota(self):
+                self.IngresoDueño = IngresoMascota()
+
+class IngresoDueño(tk.Toplevel):
+
+        def __init__(self):
+                super().__init__()
+                # configuración de la ventana 
+                self.title('Logi Vet Ingreso de nuevo dueño.')
+                self.geometry('200x400')
+
+                # MARCO
+                self.card = ttk.Frame(self, style='Card', padding=(5, 6, 7, 8))
+                self.card.pack(pady = ESPACIO_Y)
+
+                #ENTRY
+                EntryNombre = EntryCustom(self.card, text='Nombre')
+                EntryNombre.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryApellido = EntryCustom(self.card, text='Apellido')
+                EntryApellido.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryEmail = EntryCustom(self.card, text='Email')
+                EntryEmail.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryDNI = EntryCustom(self.card, text='DNI')
+                EntryDNI.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryDirección = EntryCustom(self.card, text='Dirección')
+                EntryDirección.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryTelefono = EntryCustom(self.card, text='Teléfono')
+                EntryTelefono.pack(anchor = W, pady = ESPACIO_Y)
+
+                self.IngresarMascota = ttk.Button(self.card,
+                                                text='Confirmar Datos',  
+                                                style='Accent.TButton',
+                                                command= lambda: self.FuncionMagicaDeBackend(EntryNombre.EntradaID.get(),
+                                                                                             EntryApellido.EntradaID.get(),
+                                                                                             EntryEmail.EntradaID.get(),
+                                                                                             EntryDNI.EntradaID.get(),
+                                                                                             EntryDirección.EntradaID.get(),
+                                                                                             EntryTelefono.EntradaID.get()
+                                                                                             )
+                                                )
+                self.IngresarMascota.pack(pady = ESPACIO_Y, anchor= S)
+
+
+        def FuncionMagicaDeBackend(self, nombre, apellido, email, dni, direccion, telefono):
+                #esta función será remplazada por otra del backend.
+                #En el caso feliz se encargará de ingresar un nuevo propietario a la base de datos
+                print(nombre, apellido, email, dni, direccion, telefono)
+
+
+class IngresoMascota(tk.Toplevel):
+
+        def __init__(self):
+                super().__init__()
+                # configuración de la ventana 
+                self.title('Logi Vet Ingreso de nueva mascota.')
+                self.geometry('460x460')
+
+                # MARCO
+                self.card = ttk.Frame(self, style='Card', padding=(5, 6, 7, 8))
+                self.card.pack(pady = ESPACIO_Y)
+                #ENTRY
+                EntryNombre = EntryCustom(self.card, text='Nombre')
+                EntryNombre.pack(anchor = W, pady = ESPACIO_Y)
+
+                ComboBoxTipoAnimal = ttk.Combobox(self.card, values = ["Canino",'Felino','Equino'
+                                                                       'Rumiante','Roedor','Ave',
+                                                                       'Reptil', 'Otro'
+                                                                       ])
+                ComboBoxTipoAnimal.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryRaza = EntryCustom(self.card, text='Raza')
+                EntryRaza.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryEdad = EntryCustom(self.card, text='Edad')
+                EntryEdad.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryDniDueño = EntryCustom(self.card, text='DNI Dueño')
+                EntryDniDueño.pack(anchor = W, pady = ESPACIO_Y)
+
+                EntryDniDueño2 = EntryCustom(self.card, text='DNI segundo Dueño')
+                EntryDniDueño2.pack(anchor = W, pady = ESPACIO_Y)
+
+                self.IngresarMascota = ttk.Button(self.card,
+                                                text='Confirmar Datos',  
+                                                style='Accent.TButton',
+                                                command= lambda: self.FuncionMagicaDeBackend(EntryNombre.EntradaID.get(),
+                                                                                             EntryRaza.EntradaID.get(),
+                                                                                             EntryEdad.EntradaID.get(),
+                                                                                             EntryDniDueño.EntradaID.get(),
+                                                                                             EntryDniDueño2.EntradaID.get(),
+                                                                                             )
+                                                )
+                self.IngresarMascota.pack(pady = ESPACIO_Y, anchor= S)
+
+
+        def FuncionMagicaDeBackend(self, nombre, raza, edad, dni_dueño, dni_dueño2, telefono):
+                #esta función será remplazada por otra del backend.
+                #En el caso feliz se encargará de ingresar un nuevo paciente a la base de datos
+                print(nombre, raza, edad, dni_dueño, dni_dueño2, telefono)
+
+
+class EntryCustom(ttk.Frame):
+        def __init__(self, parent, text="", *args, **options):
+                tk.Frame.__init__(self, parent, *args, **options)
+
+                self.frame = Frame(self,borderwidth=0)
+                self.frame.grid()
+
                 self.VarTexto = tk.StringVar()
-                self.EntradaID = ttk.Entry(self.MarcoBusqueda, textvariable = self.VarTexto)
+                self.EntradaID = ttk.Entry(self.frame, textvariable = self.VarTexto)
                 self.EntradaID.pack(pady = ESPACIO_Y)
-                self.VarTexto.set("ID de mascota...")
-                self.EntradaID.bind('<Button-1>', lambda e: self.borrar_texto(e))
-                self.EntradaID.bind('<FocusOut>', lambda e: self.restaurar_texto(e))
+                self.VarTexto.set(text)
+                self.EntradaID.bind('<Button-1>', lambda e: self.borrar_texto(e, text))
+                self.EntradaID.bind('<FocusOut>', lambda e: self.restaurar_texto(e, text))
 
-        def borrar_texto(self, e):
+        def borrar_texto(self, e, text):
+                if self.VarTexto.get() == text:
                         self.VarTexto.set('')
-        def restaurar_texto(self, e):
-                        self.VarTexto.set("ID de mascota...")
 
-
+        def restaurar_texto(self, e, text):
+                if self.VarTexto.get() == '':
+                        self.VarTexto.set(text)
 
 
 if __name__ == "__main__":
         app = App()
+
         app.mainloop()

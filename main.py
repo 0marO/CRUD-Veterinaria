5f8a2d160abbee80e8ad4002d182b3035dafb1da
Omar <ooriz@fi.uba.ar>
2022-07-21 12:52:09 -0300

    Incorporada la clase (ventana) de buscar registro junto con algunas de sus funcionalidades esperadas

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ class App(tk.Tk):
                 # BOTONES
                 self.BuscarRegistro = ttk.Button(self.card,
                                                 text='Buscar registro...', 
-                                                style='Accent.TButton')
+                                                style='Accent.TButton',
+                                                command= lambda: self.buscar_un_registro())
                 self.BuscarRegistro.pack(pady = ESPACIO_Y, anchor= W)
 
                 self.EstadoIngresarDueño = False
@@ -69,6 +70,9 @@ class App(tk.Tk):
 
         def ingresar_mascota(self):
                 self.IngresoDueño = IngresoMascota()
+                
+        def buscar_un_registro(self):
+                self.IngresoDueño = BuscarRegistro()
 
 class IngresoDueño(tk.Toplevel):
 
@@ -120,7 +124,6 @@ class IngresoDueño(tk.Toplevel):
                 #En el caso feliz se encargará de ingresar un nuevo propietario a la base de datos
                 print(nombre, apellido, email, dni, direccion, telefono)
 
-
 class IngresoMascota(tk.Toplevel):
 
         def __init__(self):
@@ -130,7 +133,7 @@ class IngresoMascota(tk.Toplevel):
                 self.geometry('460x460')
 
                 # MARCO
-                self.card = ttk.Frame(self, style='Card', padding=(5, 6, 7, 8))
+                self.card = ttk.Frame(self, style='Card', padding=(5, 6, 100, 9))
                 self.card.pack(pady = ESPACIO_Y)
                 #ENTRY
                 EntryNombre = EntryCustom(self.card, text='Nombre')
@@ -166,11 +169,78 @@ class IngresoMascota(tk.Toplevel):
                                                 )
                 self.IngresarMascota.pack(pady = ESPACIO_Y, anchor= S)
 
-
         def FuncionMagicaDeBackend(self, nombre, raza, edad, dni_dueño, dni_dueño2):
                 #esta función será remplazada por otra del backend.
                 #En el caso feliz se encargará de ingresar un nuevo paciente a la base de datos
                 print(nombre, raza, edad, dni_dueño, dni_dueño2)
+
+
+class BuscarRegistro(tk.Toplevel):
+        def __init__(self):
+                super().__init__()
+                # configuración de la ventana 
+                self.title('Logi Vet Búsqueda de registros.')
+                self.geometry('700x700')
+
+                # MARCO BUSQUEDA
+                self.CartaBusqueda = ttk.Frame(self, style='Card', padding=(5, 6, 10, 9))
+                self.CartaBusqueda.place(x = ESPACIO_X*2, y = ESPACIO_Y)
+
+                # SELECCION DE BUSQUEDA
+                self.LabelBuscar = Label(self.CartaBusqueda, text= "Buscar por ")
+                self.LabelBuscar.grid(row= 0, column= 0)
+
+                ComboBuscarPor = ttk.Combobox(self.CartaBusqueda, values = ["ID Mascota",'Nombre Dueño',
+                                                                       'DNI dueño','Nombre Paciente',],
+                                               state="readonly")
+                ComboBuscarPor.current(0)
+                ComboBuscarPor.grid(row= 0, column= 1)
+
+                EntryNombre = EntryCustom(self.CartaBusqueda, text='')
+                EntryNombre.grid(row= 0, column= 2, padx= 10)
+
+                self.BtBuscar = Button(self.CartaBusqueda, text ='--->',
+                                        command= self.buscar_magicamente,
+                                        borderwidth = 0)
+                self.BtBuscar.grid(row = 0, column = 3, padx= 10)
+                
+                # MARCO DATOS
+                self.card = ttk.Frame(self, style='Card', padding=(5, 6, 100, 9))
+                self.card.place(x = ESPACIO_X*2, y = ESPACIO_Y+MARCO_DE_BUSQUEDA)
+
+                #ENTRY
+                EntryNombre = EntryCustom(self.card, text='Nombre')
+                EntryNombre.grid(row= 1, column= 0)
+
+                EntryID = EntryCustom(self.card, text='ID mascota')
+                EntryID.grid(row= 2, column= 0)
+
+                EntryRaza = EntryCustom(self.card, text='Raza')
+                EntryRaza.grid(row= 3, column= 0)
+
+                EntryEdad = EntryCustom(self.card, text='Edad')
+                EntryEdad.grid(row= 4, column= 0)
+
+                EntryDniDueño = EntryCustom(self.card, text='DNI Dueño')
+                EntryDniDueño.grid(row= 5, column= 0)
+
+                EntryEmailDueño = EntryCustom(self.card, text='dueño@ejemplo.com')
+                EntryEmailDueño.grid(row= 5, column= 1, padx= 10)
+
+                EntryTelefono = EntryCustom(self.card, text='Telefono')
+                EntryTelefono.grid(row= 5, column= 2)
+
+                EntryDniDueño2 = EntryCustom(self.card, text='DNI segundo Dueño')
+                EntryDniDueño2.grid(row= 6, column= 0)
+
+                EntryEmailDueño2 = EntryCustom(self.card, text='dueño2@ejemplo.com')
+                EntryEmailDueño2.grid(row= 6, column= 1, padx= 10)
+
+                EntryTelefono2 = EntryCustom(self.card, text='Telefono dueño 2')
+                EntryTelefono2.grid(row= 6, column= 2)
+
+        def buscar_magicamente(self):
+                pass
 
 
 class EntryCustom(ttk.Frame):
